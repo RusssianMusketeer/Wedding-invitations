d76d5d445907118d83a0cc7b17ace05512f141d9
Joselyn Judith <j.devetrieva@gmail.com>
2024-02-25 18:04:36 -0500

    Co-authored-by: Arseni Dmitriev <a.devetriev@gmail.com>

---
+++ src/components/Age/age.jsx
@@ -0,0 +1,11 @@
+import "./age.css";
+
+const Age = () => {
+    return (
+        <div>
+            <span className="age">50</span>
+        </div>
+    )
+}
+
+export default Age;
---
+++ src/components/Name/name.jsx
@@ -0,0 +1,16 @@
+import "./name.scss"
+import Age from "../Age/age";
+
+const Name = () => {
+    return (
+        
+        <div>
+            <h1 className="name">Hello</h1>
+            <Age/>
+        </div>
+
+       
+    )
+    }
+
+export default Name;
