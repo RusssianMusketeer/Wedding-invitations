48924558a0c4e8818fba71c083664424e6496345
RusssianMusketeer <a.devetriev@gmail.com>
2024-02-25 22:13:15 -0500

    progresso-dia-1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import Name from "./components/Name/name";
+import "./App.scss";
+import portrait from "./portrait.jpeg";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import Music from "./music.mp3";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Name/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="sub-Header">
+					<div>
+						<h1 className="header-name">Arseni y Joselyn</h1>
+					</div>
+					<AudioPlayer
+						autoPlay={true}
+						loop={true}
+						src={Music}
+						onPlay={(e) => console.log("onPlay")}
+						// other props here
+					/>
+					<div>
+						<img
+							className="portrait"
+							src={portrait}></img>
+					</div>
+				</div>
+			</header>
+		</div>
+	);
+};
 
 export default App;
